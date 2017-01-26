1e4d6f09589e1018d9aabc687fe56bd12b75db24
mateusz <mateuszbak3@gmail.com>
2017-01-26 16:01:48 +0200

    FIX: Ability to create a BREAK record along with WORK

--- app/Http/Controllers/RecordsController.php
+++ app/Http/Controllers/RecordsController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\ActionType;
 use App\Record;
+use App\Shop;
 use Auth;
 use Illuminate\Http\Request;
 use Log;
@@ -11,21 +12,39 @@ use Log;
 class RecordsController extends Controller
 {
 
-    public function getLoggedWorkers()
+    public function showLoggedWorkers()
     {
-        if (Auth::user()->checkRole("admin") || Auth::user()->checkRole("manager")) {
+        if (Auth::user()->checkRole("admin")) {
+            $logged_workers = Record::with('user.shops')->where('finished', false)->get();
+            $shops = Shop::all();
+        } elseif (Auth::user()->checkRole("manager")) {
+            $shops = Auth::user()->shops;
+            $logged_workers = Record::with('user.shops')->where('finished', false)->get();
+            $logged_workers = User::whereHas(['shops', 'records' => function($q){
+                $q->where('role', '!=', 'admin');
+            }])->get();
+            foreach ($logged_workers as $worker) {
+                if ($worker->shops == null) {
+                    Log::warning("Worker " . $worker->id . " has no shop assigned. Omitting.");
+                    $logged_workers->forget($worker);
+                }
+                elseif (!$shops->contains($worker->shops->first()))
+                    $logged_workers->forget($worker);
+            }
         }
+        return view('home', compact('shops', 'logged_workers'));
     }
 
     public function addStartRecord(Request $request)
     {
         Log::info("Trying to add a record for user id: " . Auth::user()->id);
+        $action_id = ActionType::where('action_name', $request->type)->first()->action_id; //request->type: WORK or BREAK
+        Log::info("Action type requested: " . $request->type . " id:" . $action_id);
+
         if (Auth::user()->checkRole("worker")) {
             if (Record::where(['user_id' => Auth::user()->id, 'finished' => false])->first() == null) {
                 $record = new Record;
-
-                $record->action_id = ActionType::where('action_name', $request->type)->first()->action_id; //request->type: WORK or BREAK
-                Log::info("Action type requested: " . $request->type . " id:" . $record->action_id);
+                $record->action_id = $action_id;
                 $record->user_id = Auth::user()->id;
                 $record->finished = false;
                 if ($record->save()) Log::info("Record successfully saved, id: " . $record->id);
@@ -57,7 +76,7 @@ class RecordsController extends Controller
             if ($record = Record::where('id', $request->input('record_id'))->first()) {
                 Log::info("Found record with id: " . $record->id);
                 $record->finished = true;
-                if($record->save()) {
+                if ($record->save()) {
                     Log::info("Record successfully finished.");
                     return response()->json([
                         'status' => true
